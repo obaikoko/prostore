64dee66169e43e86990e5cbda59ad7eda11145c3
obaikoko <jesseobinna7@gmail.com>
2025-03-05 23:01:18 +0100

    customized token with jwt callback

--- auth.ts
+++ auth.ts
@@ -58,11 +58,31 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      
 
       if (trigger === 'update') {
         session.user.name = user.name;
       }
       return session;
+    },
+
+    async jwt({ token, user, trigger, session }: any) {
+      //  assign user fields to user token
+      if (user) {
+        token.role = user.role;
+
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
