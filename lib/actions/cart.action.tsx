42dd0c356d8b537bc1cc804cd2685340c4947172
obaikoko <jesseobinna7@gmail.com>
2025-03-16 21:49:51 +0100

    price calc and add to database

--- lib/actions/cart.action.tsx
+++ lib/actions/cart.action.tsx
@@ -4,40 +4,74 @@ import { CartItem } from '@/types/index';
 import { convertToPlainObject, formatError } from '../utils';
 import { auth } from '@/auth';
 import { prisma } from '@/db/prisma';
-import { cartItemSchema } from '../validators';
+import { cartItemSchema, insertCartSchema } from '../validators';
+import { round2 } from '../utils';
+import { revalidatePath } from 'next/cache';
+
+// Calculate cart prices
+const calcPrice = (items: CartItem[]) => {
+  const itemsPrice = round2(
+    items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
+  );
+  const shippingPrice = round2(itemsPrice > 100 ? 0 : 10);
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
 
 export const addItemToCart = async (data: CartItem) => {
   try {
-    // Check for Cart Cookie
+    // check for cookies
     const sessionCartId = (await cookies()).get('sessionCartId')?.value;
-    if (!sessionCartId) {
-      throw new Error('Session Cart Id not found');
-    }
+    if (!sessionCartId) throw new Error('Session cart not found');
+    // Get session UserID
 
-    // Get session and user ID
     const session = await auth();
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
-    // const cart = await getMyCart();
 
-    // Parse and validate item
+    // get cart
+    const cart = await getMyCart();
 
+    // Parse and validate items
     const item = cartItemSchema.parse(data);
-    // Get product from DB
+
+    // find product in DB
     const product = await prisma.product.findFirst({
       where: { id: item.productId },
     });
 
-    console.log({
-      sessionCartId: sessionCartId,
-      userId: userId,
-      itemRequested: item,
-      productFound: product,
-    });
-    return {
-      success: true,
-      message: 'Item added to cart',
-      data,
-    };
+    if (!product) throw new Error('Product not found');
+    if (!cart) {
+      // Create new cart
+      const newCart = insertCartSchema.parse({
+        sessionCartId: sessionCartId,
+        userId: userId,
+        items: [item],
+        ...calcPrice([item]),
+      });
+
+      // Add to DB
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+      return {
+        success: true,
+        message: 'Item added to cart',
+        data,
+      };
+    }
+
+    
+
+    
   } catch (error) {
     return {
       success: false,
@@ -47,31 +81,29 @@ export const addItemToCart = async (data: CartItem) => {
 };
 
 export const getMyCart = async () => {
-  // Check for Cart Cookie
+  // check for cookies
   const sessionCartId = (await cookies()).get('sessionCartId')?.value;
-  if (!sessionCartId) {
-    throw new Error('Session Cart Id not found');
-  }
+  if (!sessionCartId) throw new Error('Session cart not found');
 
-  // Get session and user ID
+  // get session and User ID
   const session = await auth();
   const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
-  // get user cart from database
+  // get user Cart from DB
   const cart = await prisma.cart.findFirst({
     where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
   });
 
-  if (!cart) {
-    return undefined;
-  }
+  if (!cart) return undefined;
+
+  // convert to decimals and returm
 
   return convertToPlainObject({
     ...cart,
     items: cart.items as CartItem[],
     itemsPrice: cart.itemsPrice.toString(),
-    totalPrice: cart.totalPrice.toString(),
     taxPrice: cart.taxPrice.toString(),
     shippingPrice: cart.shippingPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
   });
 };
